8d369e818de866f11845c5ee66f0ef5af6f80998
khankadir193 <abdul.kadir2019@vitbhopal.ac.in>
2024-02-15 11:47:36 +0530

    search bar

---
+++ src/Components/HomeComponent.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Search from './Search'
+
+const HomeComponent = () => {
+  return (
+    <div>
+        <Search />
+    </div>
+  )
+}
+
+export default HomeComponent
---
+++ src/Components/Search.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './CompDesign.css';
+
+
+const Search = () => {
+    return (
+        <>
+            <h1>SnapShot</h1>
+            <div className='search-container'>
+                <input type='text' placeholder='search your image' />
+                <button>Search</button>
+            </div>
+        </>
+
+    )
+}
+
+export default Search
